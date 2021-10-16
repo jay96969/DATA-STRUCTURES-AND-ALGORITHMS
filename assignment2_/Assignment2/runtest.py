bb6c354adefbf14d739f523fad18e88cd0f56ce0
Jay Kishan <79415710+jay96969@users.noreply.github.com>
2021-10-17 00:08:47 +0530

    Delete assignment2_/Assignment2 directory

--- assignment2_/Assignment2/runtest.py
+++
@@ -1,9 +0,0 @@
-import os, filecmp
-
-os.system("make compile")
-os.system("make run")
-r = filecmp.cmp("tests/output.txt", "tests/sample_out.txt")
-if r is False:
-    print("Fail")
-else:
-    print("Pass")
